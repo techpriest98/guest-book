03c9ee6e0e04bca4a50c43cf6ef1bc57f6a4f6a3
Andrii Yeromenko <andrii.yeromenko@pitchbook.com>
2024-03-05 15:46:54 +0200

    GB-14 fix controller advice

--- infrastructure/rest-api/src/main/java/pb/guestbook/exception/ControllerAdvice.java
+++ infrastructure/rest-api/src/main/java/pb/guestbook/exception/ControllerAdvice.java
@@ -1,33 +1,22 @@
 package pb.guestbook.exception;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.lang.NonNull;
-import org.springframework.security.web.firewall.RequestRejectedException;
-import org.springframework.security.web.firewall.RequestRejectedHandler;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.io.IOException;
-
 @RestControllerAdvice
-public class ControllerAdvice implements RequestRejectedHandler {
-    private static final String BAD_REQUEST = "Bad request";
-
-    @ExceptionHandler({ValidationException.class})
+public class ControllerAdvice {
+    @ExceptionHandler(ValidationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorDto adminValidationException(@NonNull ValidationException e) {
+    public ErrorDto validationException(@NonNull ValidationException e) {
         return new ErrorDto(HttpStatus.BAD_REQUEST.value(), e.getMessage());
     }
 
-    @Override
-    public void handle(
-        HttpServletRequest request,
-        HttpServletResponse response,
-        RequestRejectedException requestRejectedException
-    ) throws IOException {
-        response.sendError(HttpStatus.BAD_REQUEST.value(), BAD_REQUEST);
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.SERVICE_UNAVAILABLE)
+    public ErrorDto unavailableException(@NonNull Exception e) {
+        return new ErrorDto(HttpStatus.SERVICE_UNAVAILABLE.value(), e.getMessage());
     }
 }
